1a68180b919759304419ce91ce16669aaa47d815
Rhys Edwards <rhysrhys123@aol.com>
2023-11-28 12:22:33 +0000

    Delete EcoSwap Landing directory

--- EcoSwap Landing/js/script.js
+++
@@ -1,8 +0,0 @@
-const accordion = document.getElementsByClassName('faq-accordion-item');
-
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-
-  })
-}
